3f00549bed42057da7fa2daed512e4f124d84056
TrapenokVictor <Levhav@yandex.com>
2025-02-03 15:06:46 +0500

    Add video iframe

--- packages/store/components/unifie/UnifieDeploying.tsx
+++ packages/store/components/unifie/UnifieDeploying.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from 'next-i18next';
-import { Result } from 'antd';
+import { Result, Space } from 'antd';
 
 /**
  * UI for the deploying state of an application
@@ -11,10 +11,22 @@ import { Result } from 'antd';
 export const UnifieDeploying = () => {
   const { t } = useTranslation('unifie');
   return (
-    <Result
-      status="success"
-      title={t('unifie-deploying-title')}
-      subTitle={t('unifie-deploying-message')}
-    />
+    <>
+      <Result
+        status="success"
+        title={t('unifie-deploying-title')}
+        subTitle={t('unifie-deploying-message')}
+      />
+      <iframe
+        width="100%"
+        height="420"
+        src="https://www.youtube.com/embed/O2SMceQucJg?si=DNyuX7ln1my0Dxpq"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
+    </>
   );
 };
